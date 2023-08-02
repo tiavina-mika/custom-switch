e1537456e556f933e57c0737c458177241b02855
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-08-02 16:03:50 +0300

    clean code

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -30,7 +30,6 @@ const classes = {
       },
       "&.Mui-focusVisible .MuiSwitch-thumb": {
         color: "#33cf4d"
-        // border: '6px solid #fff',
       },
       "&.Mui-disabled .MuiSwitch-thumb": {
         color:
@@ -43,7 +42,6 @@ const classes = {
       }
     },
     "& .MuiSwitch-thumb": {
-      // boxSizing: 'border-box',
       backgroundColor: theme.palette.grey[300],
       width: 13.5,
       height: 13.5
